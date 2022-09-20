d5fbcc74e1620c585fdd834ddd88e1cadb77fa3f
deanwraith <dean.wraith@gmail.com>
2022-09-20 03:32:04 +0000

    added stripe styling and js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LjwtMLFHpEXyrk609T1Y6fbdd0pmV2pmZEgu8JVqIUZBK5qPU0ckwDqPZWluFD9ZSDHfszdeNQ7tmRKJEvRpYvv003TCwUW3U',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
